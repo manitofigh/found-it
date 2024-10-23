a0f82339e704097b6e54896b6e67a108e4003e3d
manitofigh <manitofigh@protonmail.com>
2024-10-23 16:16:21 -0400

    nativewind setup and version err fix

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,14 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View } from 'react-native';
 import { Link } from 'expo-router';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>FoundIt landing page</Text>
-      <Link href='/profile' style={{ color:"blue" }}> Go to the profile page </Link>
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-3xl font-bold">Test page</Text>
+      <Link href='/profile' className="mt-4 text-blue-500">
+        Go to the profile page
+      </Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
-
