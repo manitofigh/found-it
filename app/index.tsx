fca0a60b39a4152688e8cf19883a0953d5714e22
manitofigh <manitofigh@protonmail.com>
2024-10-23 15:38:01 -0400

    prettier.rc and profile page setup

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,21 @@
-import { Text, View } from "react-native";
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
 
-export default function Index() {
+export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text>FoundIt landing page</Text>
+      <Link href='/profile' style={{ color:"blue" }}> Go to the profile page </Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
